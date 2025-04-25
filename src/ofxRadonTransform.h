55e66b33549589ecd0f9ca2396f784a1fa2bdc5f
Gromit Park <bemore.one@gmail.com>
2025-04-25 22:54:55 +0900

    Add ofxRadonTransform header file

--- src/ofxRadonTransform.h
+++ src/ofxRadonTransform.h
@@ -9,22 +9,19 @@ public:
     ofxRadonTransform();
     ~ofxRadonTransform();
     
-    // Process the input image and generate Radon transform (sinogram)
-    void setup(ofImage& inputImage);
+    // 라돈 변환 계산 함수
+    void compute(const ofImage& inputImage, ofImage& outputImage, int angleResolution = 360);
     
-    // Process with custom parameters
-    void setup(ofImage& inputImage, int numAngles);
+    // 라돈 변환 결과 가져오기
+    ofImage getRadonImage();
     
-    // Get the resulting sinogram
-    ofImage& getSinogram();
-    
-    // Get processing time in seconds
-    double getProcessingTime();
+    // 계산 시간 가져오기
+    float getElapsedTime();
     
 private:
-    ofImage sinogram;
-    double processingTime;
+    // 라돈 변환 내부 구현
+    cv::Mat computeRadonTransform(const cv::Mat& srcImage, int angleResolution);
     
-    // Core Radon transform implementation
-    void computeRadonTransform(cv::Mat& src, cv::Mat& dest, int angle);
+    ofImage radonImage;
+    float elapsedTime;
 };
