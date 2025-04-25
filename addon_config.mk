5a71635a61986e21d182d82e265c76d4114524c2
Gromit Park <bemore.one@gmail.com>
2025-04-25 22:57:19 +0900

    Update addon_config.mk

--- addon_config.mk
+++ addon_config.mk
@@ -1,46 +1,27 @@
-# All variables and paths must be defined on the command line when invoking the ofx addon template system
+# addon name
 meta:
 	ADDON_NAME = ofxRadonTransform
-	ADDON_DESCRIPTION = Radon Transform (Sinogram) implementation for Openframeworks
+	ADDON_DESCRIPTION = Radon Transform (Sinogram) addon for OpenFrameworks
 	ADDON_AUTHOR = bemoregt
-	ADDON_TAGS = "image processing" "computer vision" "radon transform" "sinogram"
+	ADDON_TAGS = "image processing" "radon transform" "sinogram" "computer vision"
 	ADDON_URL = https://github.com/bemoregt/ofxRadonTransform
 
 common:
-	# dependencies with other addons, a list of them separated by spaces 
-	ADDON_DEPENDENCIES = ofxCv ofxGui ofxOpenCv
+	# dependencies
+	ADDON_DEPENDENCIES = ofxCv ofxOpenCv ofxGui
 	
-	# include search paths, this will be usually parsed from the file system
-	# but if the addon or addon libraries need special search paths they can be
-	# specified here separated by spaces or one per line using +=
+	# include search paths
 	ADDON_INCLUDES = src
 	
-	# any special flag that should be passed to the compiler when using this
-	# addon
-	ADDON_CFLAGS = 
-	
-	# any special flag that should be passed to the linker when using this
-	# addon, also used for system libraries with -lname
-	ADDON_LDFLAGS = 
-	
-	# linux only, any library that should be included in the project using
-	# pkg-config
-	ADDON_PKG_CONFIG_LIBRARIES = 
-	
-	# osx/iOS only, any framework that should be included in the project
-	ADDON_FRAMEWORKS =
-	
-	# source files, these will be usually parsed from the file system looking
-	# in the src folders in libs and the root of the addon. if your addon needs
-	# to include files in different places or a different set of files per platform
-	# they can be specified here
+	# source search paths
 	ADDON_SOURCES = src/ofxRadonTransform.cpp
+	ADDON_SOURCES += src/ofxRadonTransform.h
 	
-	# some addons need resources to be copied to the bin/data folder of the project
-	# specify here any files that need to be copied, you can also use wildcards like
-	# images/*.png
-	ADDON_DATA = 
+	# exclude sources
+	# ADDON_SOURCES_EXCLUDE = 
 	
-	# when parsing the file system looking for libraries exclude this for all or
-	# a specific platform
-	ADDON_LIBS_EXCLUDE =
+	# header search paths
+	# ADDON_HEADER_SEARCH_PATHS =
+	
+	# frameworks to add
+	# ADDON_FRAMEWORKS =
